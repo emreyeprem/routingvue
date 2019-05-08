c39fa1b2a924dfef59fb185e388ad5783932521e
Emre Akurek <emreyeprem@gmail.com>
2019-05-08 12:30:57 -0500

    navigating pages successfully

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,6 +2,7 @@
     <div>
       <h1>The User Page</h1>
       <hr />
+      <p>Loaded ID: {{ id }}</p>
       <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 
@@ -10,6 +11,17 @@
 
 <script>
   export default {
+    data() {
+      return {
+        id: this.$route.params.id
+      }
+    },
+    watch: {
+      '$route'(to, from){
+        this.id = to.params.id;
+      }
+    }
+    ,
     methods: {
       navigateToHome(){
         this.$router.push({path: '/'})
