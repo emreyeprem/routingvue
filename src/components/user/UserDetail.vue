1afd3f8ab3e6ae8594722cb064353edc8bf920cf
Emre Akurek <emreyeprem@gmail.com>
2019-05-13 11:04:50 -0500

    navigating pages successfully

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,6 +2,6 @@
   <div>
     <h3>Some User Details</h3>
     <p>User loaded has ID: {{ $route.params.id }}</p>
-    <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'" class="btn btn-warning">Edit User</router-link>    
+    <router-link tag="button" :to="{name: 'userEdit'}" class="btn btn-warning">Edit User</router-link>    
   </div>
 </template>
