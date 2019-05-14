ffca7fcb178e238a61b5908d2d246d3eb568ad0d
Emre Akurek <emreyeprem@gmail.com>
2019-05-14 13:33:31 -0500

    navigating pages successfully

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
                 <h1>Routing</h1>
                 <hr>
                 <router-view name="header-top"></router-view>
+                <transition name="slide" mode="out-in">
                 <router-view></router-view>
+                </transition>
                 <router-view name="header-bottom"></router-view>
             </div>
         </div>
@@ -22,4 +24,32 @@
 </script>
 
 <style>
+ .slide-leave-active {
+   transition: opacity 1s ease;
+   opacity: 0;
+   animation: slide-out 1s ease-out forwards;
+ }
+ .slide-leave {
+   opacity: 1;
+   transform: translateX(0);
+ }
+ .slide-enter-active {
+   animation: slide-in 1s ease-out forwards;
+ }
+ @key-frames slide-out {
+   0% {
+     transform: translateY(0);
+    }
+   100% {
+     transform: translateY(-30px);
+   }
+ }
+ @key-frames slide-in {
+   0% {
+     transform: translateY(-30);
+    }
+  100% {
+      transform: translateY(0);
+     }
+  }
 </style>
