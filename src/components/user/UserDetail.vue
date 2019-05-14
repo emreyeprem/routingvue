d5b028ce2ec7682347d6c92f881d2e59efee177d
Emre Akurek <emreyeprem@gmail.com>
2019-05-14 13:56:20 -0500

    navigating pages successfully

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,7 +2,23 @@
   <div>
     <h3>Some User Details</h3>
     <p>User loaded has ID: {{ $route.params.id }}</p>
-    <router-link tag="button" :to="{name: 'userEdit', params: {id: $route.params.id}, query: {locale: 'en', q: 100}}"
-                 class="btn btn-warning">Edit User</router-link>    
+    <router-link tag="button" :to="link"
+                 class="btn btn-warning">Edit User</router-link>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        link: {
+        name: 'userEdit',
+        params: {id: this.$route.params.id},
+        query: {locale: 'en', q: 100},
+        hash: '#data'
+        }
+      }
+    }
+  }
+
+</script>
